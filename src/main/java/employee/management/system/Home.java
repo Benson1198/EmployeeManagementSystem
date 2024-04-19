a712df9bfe585c9f7bf21fcc96f128d4549d9245
Benson1198 <bensonpx1198@gmail.com>
2024-04-19 23:43:30 +0530

    Update

--- src/main/java/employee/management/system/Home.java
+++ src/main/java/employee/management/system/Home.java
@@ -61,7 +61,7 @@ public class Home extends JFrame implements ActionListener {
             new ViewEmployee();
         } else {
             setVisible(false);
-//            new RemoveEmployee();
+            new RemoveEmployee();
         }
     }
 }
